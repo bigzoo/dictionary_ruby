3a9f16b82103c974a5406919b56abc79b5554b12
Chris Nyaga <nyagachris75@gmail.com>
2017-07-08 13:23:24 +0300

    Rake file updated to run all rspec tests automatically.

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,9 @@
-# require('bundler/gem_tasks')
-# require('rspec/core/rake_task')
-# RSpec::Core::RakeTask.new
-task default: puts do
-  'qwerty'
+begin
+  require 'rspec/core/rake_task'
+
+  RSpec::Core::RakeTask.new(:spec)
+
+  task default: :spec
+rescue LoadError
+  puts 'No rspec available'
 end
